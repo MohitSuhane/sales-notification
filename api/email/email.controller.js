f45ea122fa042300f3039f0911d1872d4382570b
Mohit Suhane <mohit.suhane@imaginea.com>
2019-01-12 17:16:38 +0530

    Send email

--- api/email/email.controller.js
+++ api/email/email.controller.js
@@ -1,21 +1,33 @@
-const emailController = {
-  sendEmail: async (req, res) => {
-    try {
-    //   const { accessToken, deploy, axiosWrapper } = getDataFromReq(req)
-    //   const { dealUidsCsv, nominationDate } = req.query
-    //   if (!dealUidsCsv || !nominationDate) {
-    //     util.response.status(RestStatus.BAD_REQUEST, res, 'Mandatory parameters dealUid, nominationDate')
-    //     return;
-      }
-    //   const dealUids = dealUidsCsv.split(',');
+// using SendGrid's v3 Node.js Library
+// https://github.com/sendgrid/sendgrid-nodejs
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-    //   const nominationBatches = await getAllNominationBatches(dealUids, nominationDate, { accessToken, deploy, axiosWrapper })
-    //   util.response.status200(res, nominationBatches);
-     catch (_err) {
-    //   const err = universalError(_err, req.originalUrl);
-    //   util.response.status(err.statusCode, res, err.message);
-    }
-  },
+const { sender, subject } = require('../utils/utils');
+
+const emailController = {
+    sendEmail: async (req, res) => {
+        const { emailId, saleIdentifier, lastBoughtItem, userName } = req.body;
+        try {
+            const msg = {
+                to: emailId,
+                from: sender,
+                subject: 'Sending with SendGrid is Fun',
+                text: 'and easy to do anywhere, even with Node.js',
+                html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+            };
+            await sgMail.send(msg);
+            res.status(200).send({
+                success: true, message: `Email Successfully Sent to user ${emailId}`
+            });
+        }
+        catch (err) {
+            console.log("Send Email failed:", err);
+            res.status(500).send({
+                success: false, message: `Server Error ${err}`
+            });
+        }
+    },
 };
 
 module.exports = emailController;
---
+++ api/utils/utils.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+const sender = 'suhane1989@gmail.com';
+const subject = 'Red Hot Winter Sale is Onn!!!'
+const cwd = path.resolve(process.cwd());
+
+module.exports = {
+    sender,
+    cwd,
+}
